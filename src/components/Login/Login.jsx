541493e6a5eb9958cd66b47cd2ec165ebd338d77
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-16 21:11:08 +0600

    working on login page design

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,21 @@
 import LoginWithEmail from "./LoginWithEmail";
 import LoginWithSocial from "./LoginWithSocial";
+import divider from "../../assets/underline.svg"
 import "../../style/login.css"
 
 function Login() {
     return (
-        <div className="text-center">
+        <div className="text-center mt-20">
             <div className="loginTextHolder">
                 <h2 className="signInText">Sign In</h2>
                 <p className="welcomeText">Welcome back, you’ve been missed!</p>
             </div>
             <LoginWithSocial />
+            <div className="flex items-center space-x-6 justify-center">
+                <img src={divider} alt="" />
+                <p className="or">OR</p>
+                <img src={divider} alt="" />
+            </div>
             <LoginWithEmail />
         </div>
     )
