806eb62d5784fdf4fa887ede218bcd42c0e3cf14
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-17 03:51:15 +0600

    working on login:input box over icon problem solved

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,9 +1,14 @@
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { BiLockOpen } from "react-icons/bi";
+import { GoEye } from "react-icons/go";
+import { GoEyeClosed } from "react-icons/go";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import IsEmailValid from "../utils/IsEmailValid";
 
 function LoginForm() {
     const [formData, setFormData] = useState({ email: "", password: "" });
+    const [open, setOpen] = useState(false)
 
     const handleFormData = (event) => {
         event.preventDefault();
@@ -13,30 +18,64 @@ function LoginForm() {
 
     }
 
+    console.log(IsEmailValid(formData?.email))
     return (
         <form onSubmit={handleSubmit}>
             <div className="flex flex-col items-center space-y-12">
+
                 <div className="relative">
-                    {formData?.email !== "" || <div className="absolute top-2 left-4 flex items-center space-x-1">
-                        <MdOutlineAlternateEmail className="placeHolderIcon" />
-                        <span className="label">Your Email</span>
-                    </div>}
-                    <input type="text" placeholder="" className="inputBox"
+                    {formData?.email !== "" ||
+                        <div className="absolute top-3 left-4 flex items-center space-x-1">
+                            <MdOutlineAlternateEmail className="placeHolderIcon" />
+                            <span className="label">Your Email</span>
+                        </div>
+                    }
+
+                    <input type="text"
+                        className={`inputBox`}
                         name="email"
                         onChange={(event) => handleFormData(event)}
+                        required
                     />
+                    {formData?.email&& IsEmailValid(formData?.email) === null ? <small className="invalidEmailError block text-start">Please enter a valid email address.</small> : ""}
                 </div>
+                {/* Password */}
                 <div className="relative">
-                    {formData?.password !== "" || <div className="absolute top-2 left-4 flex items-center space-x-1">
-                        <BiLockOpen className="placeHolderIcon" />
-                        <span className="label">Password</span>
-                    </div>}
+                    {formData?.password !== "" ||
+                        <div className="absolute top-3 left-4 flex items-center space-x-1">
+                            <BiLockOpen className="placeHolderIcon" />
+                            <span className="label">Password</span>
+                        </div>
+                    }
 
-                    <input type="password" placeholder="" className="inputBox"
+                    <input type={open ? "text" : "password"}
+                        className="inputBox"
                         name="password"
-                        onChange={(event) => handleFormData(event)} />
+                        onChange={(event) => handleFormData(event)}
+                        required
+                    />
+
+                    {/* password be showed or not be showed */}
+                    <p className="absolute top-4 right-5 cursor-pointer">
+                        {open ? <GoEye className="eye" onClick={() => setOpen(!open)} /> : <GoEyeClosed className="eye" onClick={() => setOpen(!open)} />}
+                    </p>
+
+                    {/* Checkbox */}
+                    <div className="mt-4">
+                        <p className="label flex items-center space-x-2 justify-start">
+                            <input type="checkbox" className="checkbox cursor-pointer" />
+                            <span className="checkBoxText">Remember me</span>
+                        </p>
+                    </div>
+
+                    {/* Form button */}
+                    <div className="signInBtn">
+                        <button className="signInBtnText">Sign In</button>
+                    </div>
+                    <p className="formFooter">Don’t have an account yet? <Link className="formFooterLink" to="/signup">Sign Up</Link></p>
                 </div>
             </div>
+
         </form>
     );
 }
