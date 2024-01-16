7835e66aecbbf7a78ec3c67f0fc66e28baf09fd2
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-17 02:59:44 +0600

    working on login:input box over icon problem solved

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,23 +1,40 @@
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { BiLockOpen } from "react-icons/bi";
+import { useState } from "react";
 
 function LoginForm() {
+    const [formData, setFormData] = useState({ email: "", password: "" });
+
+    const handleFormData = (event) => {
+        event.preventDefault();
+        setFormData(prev => (setFormData({ ...prev, [event.target.name]: event.target.value })))
+    }
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="flex flex-col items-center space-y-12">
                 <div className="relative">
-                    <div className="absolute top-2 left-4 flex items-center space-x-1">
+                    {formData?.email !== "" || <div className="absolute top-2 left-4 flex items-center space-x-1">
                         <MdOutlineAlternateEmail className="placeHolderIcon" />
-                        <span className="mb-1 label">Your Email</span>
-                    </div>
-                    <input type="text" placeholder="" className="inputBox" />
+                        <span className="label">Your Email</span>
+                    </div>}
+                    <input type="text" placeholder="" className="inputBox"
+                        name="email"
+                        onChange={(event) => handleFormData(event)}
+                    />
                 </div>
-                <div>
-                    <div>
+                <div className="relative">
+                    {formData?.password !== "" || <div className="absolute top-2 left-4 flex items-center space-x-1">
                         <BiLockOpen className="placeHolderIcon" />
                         <span className="label">Password</span>
-                    </div>
-                    <input type="password" placeholder="Password" className="inputBox" />
+                    </div>}
+
+                    <input type="password" placeholder="" className="inputBox"
+                        name="password"
+                        onChange={(event) => handleFormData(event)} />
                 </div>
             </div>
         </form>
