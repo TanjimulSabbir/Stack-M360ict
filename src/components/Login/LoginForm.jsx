cabf0e320ac9f363ee712c18fb2e25cbea5136e5
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-17 02:50:15 +0600

    working on login page design

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,23 +1,27 @@
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { BiLockOpen } from "react-icons/bi";
-import { useState } from "react";
 
 function LoginForm() {
-    const [open, setOpen] = useState(true)
     return (
         <form>
             <div className="flex flex-col items-center space-y-12">
                 <div className="relative">
-                    {open && <MdOutlineAlternateEmail className="absolute placeHolderIcon top-4 left-2" />}
-                    <input type="text" placeholder="Your Email" className="inputBox" onChange={()=>setOpen(false)} />
-                </div>
-                <div className="relative">
+                    <div className="absolute top-2 left-4 flex items-center space-x-1">
+                        <MdOutlineAlternateEmail className="placeHolderIcon" />
+                        <span className="mb-1 label">Your Email</span>
+                    </div>
                     <input type="text" placeholder="" className="inputBox" />
-                    {open &&<div className="absolute placeHolderIcon top-4 left-2"> <BiLockOpen  /> <span>Password</span></div>}
+                </div>
+                <div>
+                    <div>
+                        <BiLockOpen className="placeHolderIcon" />
+                        <span className="label">Password</span>
+                    </div>
+                    <input type="password" placeholder="Password" className="inputBox" />
                 </div>
             </div>
         </form>
-    )
+    );
 }
 
 export default LoginForm;
