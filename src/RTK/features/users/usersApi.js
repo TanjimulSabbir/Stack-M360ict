6e4df05c415c598031bff4be1b70a6301b44d905
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-18 07:18:28 +0600

    working on dashboard page:user logic part

--- src/RTK/features/users/usersApi.js
+++ src/RTK/features/users/usersApi.js
@@ -3,7 +3,7 @@ import { apiSlice } from "../api/apiSlice";
 export const usersApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getUsers: builder.query({
-            query: (page = 2) => ({
+            query: (page = 1) => ({
                 url: `/users?per_page=10&page=${page}`
             })
         }),
--- src/pages/dashboard/Users.jsx
+++ src/pages/dashboard/Users.jsx
@@ -1,7 +1,19 @@
+import { ErrorIcon, LoaderIcon } from "react-hot-toast";
+import { useGetUsersQuery } from "../../RTK/features/users/usersApi";
+
 function Users() {
-  return (
-    <div>Users</div>
-  )
+    const { data, isLoading, isError, error } = useGetUsersQuery();
+    let content = null;
+    if (isLoading) content = <LoaderIcon />
+    if (!isLoading && isError) content = <ErrorIcon />
+    if (!isLoading && !isError && data?.data.length > 0) {
+        content = data?.data.map(user=>user.email)
+    }
+    return (
+        <div>
+            {content}
+        </div>
+    )
 }
 
 export default Users;
